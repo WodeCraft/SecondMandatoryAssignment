43ebe763813acebf10c9bd613239b1eaa79b8c64
Jens Christian Rasch <jcr@wodecraft.dk>
2016-10-13 10:16:48 +0200

    Exercise 2

---
+++ MbmStore/ViewModels/CartLine.cs
@@ -0,0 +1,10 @@
+﻿using MbmStore.Models;
+
+namespace MbmStore.ViewModels
+{
+    public class CartLine
+    {
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
